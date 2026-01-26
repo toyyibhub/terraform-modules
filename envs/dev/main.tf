9cc1ba08501140dd4b6c4c023d9bf141456fb2ab
Yinka Salau <salaudeentoyyib@gmail.com>
2026-01-26 13:13:01 -0600

    adding new envs for terraform

---
+++ envs/dev/main.tf
@@ -0,0 +1,15 @@
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami_id         = "ami-0532be01f26a3de55"
+  instance_type = "t2.micro"
+  instance_name = "dev-ec2"
+  env            = var.env
+}
+
+module "s3" {
+  source = "../../modules/s3"
+
+  bucket_name = "my-simple-dev-bucket-12345"
+  env         = var.env
+}
