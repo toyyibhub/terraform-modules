d6dfa8aae135cf9569b695de567cc6c58cd02586
Yinka Salau <salaudeentoyyib@gmail.com>
2026-01-26 13:32:44 -0600

    adding new ec2

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -13,3 +13,13 @@ module "s3" {
   bucket_name = "my-simple-dev-bucket-12345"
   env         = var.env
 }
+
+
+module "secondec2" {
+  source = "../../modules/ec2"
+
+  ami_id         = "ami-0532be01f26a3de55"
+  instance_type = "t2.micro"
+  instance_name = "dev2-ec2"
+  env            = var.env
+}
